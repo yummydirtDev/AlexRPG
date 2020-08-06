5ea829024ffad5d5a5c1bc3c0748fad9cb5f299d
Alex Frutkin <frutkinalexander@gmail.com>
2020-08-05 17:16:33 -0700

    Set up saveload environment

---
+++ alexsaveload/saveload.h
@@ -0,0 +1,9 @@
+#ifndef __SAVELOAD_H__
+#define __SAVELOAD_H__
+
+// Used for input and output in the save/load function
+#include <iostream>
+// Used to save and load the data from the file
+#include <fstream>
+
+#endif // __SAVELOAD_H__
