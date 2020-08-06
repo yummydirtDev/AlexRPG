fbc93fc3c00d867041d78e503adcb62fea9d8708
Alex Frutkin <frutkinalexander@gmail.com>
2020-08-06 16:22:46 -0700

    added to the init and fixed some bugs

--- game/init.cpp
+++ game/init.cpp
@@ -6,22 +6,33 @@ void initGame() {
     char newOrOld;
     std::string confirm;
     std::vector<std::string> blankVector{"0"};
+    bool inputRecognized{false};
 
     std::cout << "Welcome to AlexRPG" << '\n';
-    std::cout << "Would you like to (l)oad a save or start a (n)ew adventure? ";
-    std::cin >> newOrOld;
-    std::cout << '\n';
-    switch (newOrOld) {
-        case 'l':
-        case 'L':
-            std::cout << "This feature has not been implemented yet, come back soon!" << std::endl;
-            break;
-        case 'n':
-        case 'N':
-            std::cout << "This will erase your current save data. \n Type confirm to confirm. \n";
-            std::cin >> confirm;
-            if (confirm == "confirm") {
-                saveGame(blankVector);
-            }
+    while (!inputRecognized) {
+        std::cout << "Would you like to (l)oad a save or start a (n)ew adventure? ";
+        std::cin >> newOrOld;
+        std::cout << '\n';
+        switch (newOrOld) {
+            case 'l':
+            case 'L':
+                std::cout << "This feature has not been implemented yet, come back soon!" << std::endl;
+                break;
+            case 'n':
+            case 'N':
+                std::cout << "This will erase your current save data. \nType confirm to confirm. \n";
+                std::cin >> confirm;
+                if (confirm == "confirm" || confirm == "Confirm") {
+                    saveGame(blankVector);
+                    std::cout << "Successfully overwrote the save file!\n";
+                    inputRecognized = true;
+                    break;
+                }
+                std::cout << "Confirm not typed" << '\n';
+            default:
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                std::cout << "Input not recognized, trying again." << std::endl;
+        }
     }
 }
