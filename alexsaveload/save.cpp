e50c381a15f0d487316635a054ff1bef21d66d13
Alex Frutkin <frutkinalexander@gmail.com>
2020-08-05 17:53:25 -0700

    work on the save function

--- alexsaveload/save.cpp
+++ alexsaveload/save.cpp
@@ -1,5 +1,14 @@
 #include "saveload.h"
 
-void saveGame(std::vector<auto> dataList) {
-    
+void saveGame(std::vector<boost::variant<float, int>> dataList) {
+    for (int i{0}; i < dataList.size(); i++) {
+        std::cout << dataList[i] << '\n';
+    }
+}
+
+int main() {
+    int thingy{12};
+    float f{0.233};
+    std::vector<boost::variant<float, int>> list{thingy, f};
+    saveGame(list);
 }
