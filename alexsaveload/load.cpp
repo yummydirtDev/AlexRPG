4898823e37341f686acca548e840ba620829c980
Alex Frutkin <frutkinalexander@gmail.com>
2020-08-05 18:57:14 -0700

    added the current variable

--- alexsaveload/load.cpp
+++ alexsaveload/load.cpp
@@ -2,5 +2,6 @@
 
 std::vector<std::string> loadGame() {
     std::vector<std::string> loadList{};
+    std::string currentItem{""};
     std::ofstream saveFile{std::string(getenv("HOME"))+"/.AlexRPG"};
 }
