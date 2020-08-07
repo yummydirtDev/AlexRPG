7d1b664865376b0f0fd8f07cf2ce45a73105adfb
Alex Frutkin <frutkinalexander@gmail.com>
2020-08-06 21:55:37 -0700

    removed a test

--- game/main.cpp
+++ game/main.cpp
@@ -1,11 +1,8 @@
 #include "main.h"
 
 int main() {
-    std::vector<int> tems{0};
-    GameData penis{1, tems, 1, 1, 1, 1};
     // Intro and initialize
     initGame();
-    std::cout << penis.getCurrentHealth();
     // Game loop
         // check state
         // do move
