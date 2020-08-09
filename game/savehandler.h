181865c09ef072d642df426fec1e66f40102d10f
Alex Frutkin <frutkinalexander@gmail.com>
2020-08-09 13:12:27 -0700

    added game status indicator for the game loop

--- game/savehandler.h
+++ game/savehandler.h
@@ -18,6 +18,7 @@ private:
 public:
     GameData(std::string userName, int turns, std::vector<int> items, int maxHealth, int currentHealth, int wit, int stoneface, int xp, int level);
     GameData();
+    int gameStatus;
     std::vector<int> m_items;
     std::string getUserName();
     int getTurns();
