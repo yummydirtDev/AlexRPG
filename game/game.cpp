7073364d2a4f8dc11eb456cc105d6646cc3f227e
Alex Frutkin <frutkinalexander@gmail.com>
2020-08-09 12:48:21 -0700

    put a skeleton of the tutorial

--- game/game.cpp
+++ game/game.cpp
@@ -1,9 +1,26 @@
 #include "game.h"
 
 GameData gameLoop(GameData data) {
+    char yOrN;
+    bool inputRecognized{ false };
     switch (data.getTurns()) {
     case 0:
         std::cout << "You appear to be new to AlexRPG, would you like to hear how things work? (Y or N) ";
+        while (!inputRecognized) {
+            std::cin >> yOrN;
+            switch (yOrN) {
+            case 'y':
+            case 'Y':
+                inputRecognized = true;
+                break;
+            case 'n':
+            case 'N':
+                inputRecognized = true;
+                break;
+            default:
+                break;
+            }
+        }
     }
     return data;
 }
