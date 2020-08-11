c8695420cee0cf39029eaa9305e30396ab7bcf22
Alex Frutkin <frutkinalexander@gmail.com>
2020-08-10 22:16:18 -0700

    moved the tutorial over to a function

--- game/game.cpp
+++ game/game.cpp
@@ -1,5 +1,15 @@
 #include "game.h"
 
+static inline void tutorial(GameData *data) {
+    std::cout << "Alright! AlexRPG is an RPG where you play as Alex, on a journey to " <<
+        "destroy Googular, a biting allegory for corporate America.\n";
+    std::cout << "Rather than fight with weapons, you tell jokes. " <<
+        "The measure of joke-telling ability is called wit. To start out you have " <<
+        data->getWit() << " wit.\n";
+    std::cout << "While there is no real violence from the attacking side, these jokes cause real damage. " <<
+        "The defending side will have real and gruesome injuries from these jokes.";
+}
+
 GameData gameLoop(GameData data) {
     char yOrN;
     bool inputRecognized{ false };
@@ -12,11 +22,7 @@ GameData gameLoop(GameData data) {
             case 'y':
             case 'Y':
                 inputRecognized = true;
-                std::cout << "Alright! AlexRPG is an RPG where you play as Alex, on a journey to " <<
-                    "destroy Googular, a biting allegory for corporate America.\n";
-                std::cout << "Rather than fight with weapons, you tell jokes. " <<
-                    "The measure of joke-telling ability is called wit. To start out you have " <<
-                    data.getWit() << " wit.\n";
+                tutorial(&data);
                 break;
             case 'n':
             case 'N':
