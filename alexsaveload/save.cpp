ba26bba57f54f560d67ef82efeeca3f304c1ce8c
Alex Frutkin <frutkinalexander@gmail.com>
2020-08-15 09:35:37 -0700

    added windows save support

--- alexsaveload/save.cpp
+++ alexsaveload/save.cpp
@@ -1,7 +1,23 @@
 #include "saveload.h"
 
+#ifdef __linux__
+    std::string getSavePath() {
+        return std::string(getenv("HOME"))+"/.AlexRPG";
+    }
+#endif
+#ifdef _WIN32
+    std::string getSavePath() {
+        std::string path{getenv("APPDATA")};
+        std::cout << path;
+        path = path + "\\AlexRPG\\.AlexRPG";
+        std::cout << path;
+        return path;
+    }
+#endif
+
 void saveGame(std::vector<std::string> dataList) {
-    std::ofstream saveFile{ std::string(getenv("HOME"))+"/.AlexRPG" };
+    getSavePath();
+    std::ofstream saveFile{ getSavePath() };
     for (int i{ 0 }; i < dataList.size(); i++) {
         saveFile << dataList[i] << '\n';
     }
