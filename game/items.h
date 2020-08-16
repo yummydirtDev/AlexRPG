2010bd669b540ab096ce1002810f372309acac7f
Alex Frutkin <frutkinalexander@gmail.com>
2020-08-15 22:15:47 -0700

    finished item class and added constructor

--- game/items.h
+++ game/items.h
@@ -9,6 +9,17 @@ private:
     int m_type;
     std::string m_name;
     int m_healing;
+    int m_damage;
+    int m_cost;
+public:
+    Item(int id, int type, std::string name, int healing, int damage, int cost) {
+        m_id = id;
+        m_type = type;
+        m_name = name;
+        m_healing = healing;
+        m_damage = damage;
+        m_cost = cost;
+    }
 };
 
 #endif // __ITEMS_H__
