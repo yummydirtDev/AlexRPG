967c9f5403ac587586cd31fcbe5d3b48b968de61
Alex Frutkin <frutkinalexander@gmail.com>
2020-08-15 20:54:35 -0700

    added more stats

--- game/items.h
+++ game/items.h
@@ -1,11 +1,14 @@
 #ifndef __ITEMS_H__
 #define __ITEMS_H__
 
+#include <string>
+
 class Item {
 private:
     int m_id;
     int m_type;
-    int m_name;
+    std::string m_name;
+    int m_healing;
 };
 
 #endif // __ITEMS_H__
