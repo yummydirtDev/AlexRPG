edb5b85a01b71ae66eeec7378e0b5328911920cb
Alex Frutkin <frutkinalexander@gmail.com>
2020-08-24 11:44:29 -0700

    added public variables and functions

--- game/combat.h
+++ game/combat.h
@@ -1,12 +1,22 @@
 #ifndef __COMBAT_H__
 #define __COMBAT_H__
 
+#include <vector>
+#include <string>
+
 class Enemy {
 private:
     int m_name;
     int m_health;
     int m_wit;
     int m_stoneface;
+public:
+    std::vector<std::string> enemyLines;
+    Enemy(int name, int health, int wit, int stoneface, std::vector<std::string> lines);
+    int getName();
+    int getHealth();
+    int getWit();
+    int getStoneface();
 };
 
 #endif // __COMBAT_H__
