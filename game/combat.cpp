e82a00a51911ae5210d9f4bc50a5fbfc56d2ba7e
Alex Frutkin <frutkinalexander@gmail.com>
2020-08-24 18:53:35 -0700

    name isnt an integer

--- game/combat.cpp
+++ game/combat.cpp
@@ -1,9 +1,13 @@
 #include "combat.h"
 
-Enemy::Enemy(int name, int health, int wit, int stoneface, std::vector<std::string> lines) {
+Enemy::Enemy(std::string name, int health, int wit, int stoneface, std::vector<std::string> lines) {
     m_name = name;
     m_health = health;
     m_wit = wit;
     m_stoneface = stoneface;
     enemyLines = lines;
+}
+
+std::string Enemy::getName() {
+    return m_name;
 }
--- game/combat.h
+++ game/combat.h
@@ -6,15 +6,15 @@
 
 class Enemy {
 private:
-    int m_name;
+    std::string m_name;
     int m_health;
     int m_wit;
     int m_stoneface;
     int m_xpGiven;
 public:
     std::vector<std::string> enemyLines;
-    Enemy(int name, int health, int wit, int stoneface, std::vector<std::string> lines);
-    int getName();
+    Enemy(std::string name, int health, int wit, int stoneface, std::vector<std::string> lines);
+    std::string getName();
     int getHealth();
     int getWit();
     int getStoneface();
