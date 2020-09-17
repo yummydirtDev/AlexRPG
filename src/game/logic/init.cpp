18cd556943823d4c63f3a8a75ef312e58c370946
Alex Frutkin <frutkinalexander@gmail.com>
2020-09-17 12:04:36 -0700

    Added GPL notice

--- src/game/logic/init.cpp
+++ src/game/logic/init.cpp
@@ -1,3 +1,21 @@
+/*
+    Copyright 2020 Alex Frutkin
+    This file is part of AlexRPG.
+
+    AlexRPG is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    AlexRPG is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with AlexRPG.  If not, see <https://www.gnu.org/licenses/>.
+
+*/
 #include "init.h"
 
 static int newGame(GameData* data);
