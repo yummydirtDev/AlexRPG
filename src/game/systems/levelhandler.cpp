4840a98a35ae0fc9e1e211874f9238c1087d8e4f
Alex Frutkin <frutkinalexander@gmail.com>
2020-09-24 20:35:09 -0700

    added a level handler

---
+++ src/game/systems/levelhandler.cpp
@@ -0,0 +1,5 @@
+#include "levelhandler.h"
+
+void checkLevelUp(GameData data) {
+    std::cout << "Your level is " << data.getLevel() << '\n';
+}
---
+++ src/game/systems/levelhandler.h
@@ -0,0 +1,8 @@
+#ifndef __LEVELHANDLER_H__
+#define __LEVELHANDLER_H__
+
+#include "savehandler.h"
+
+void checkLevelUp(GameData data);
+
+#endif // __LEVELHANDLER_H__
