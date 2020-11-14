77b816bf7e7b9e6f55604e63071b62eb1a66e2ac
Alex Frutkin <frutkinalexander@gmail.com>
2020-11-14 12:28:40 -0700

    added basic shop function

---
+++ src/game/systems/store.h
@@ -0,0 +1,31 @@
+/*
+    Copyright 2020 Alex Frutkin
+    
+    This file is part of AlexRPG.
+
+    AlexRPG is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    AlexRPG is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with AlexRPG.  If not, see <https://www.gnu.org/licenses/>.
+
+*/
+#ifndef __STORE_H__
+#define __STORE_H__
+
+#include <iostream>
+#include "../lib/color.h"
+#include "savehandler.h"
+#include "items.h"
+
+// The store to buy items in
+GameData store(GameData data, Items items);
+
+#endif // __STORE_H__
